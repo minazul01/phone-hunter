7e362716dfdfea6b34a2a5b0d2a037b097e50229
minazul01 <minajul4010@gmail.com>
2024-12-28 15:33:44 +0600

    all section ok

--- index.js
+++ index.js
@@ -1,68 +1,95 @@
-/* calling api */
-const searchBtn = async(status, brandName) => {
-    console.log('wow we are done 3 second');
-    document.getElementById('spinner').style.display = 'none';
+/* spinner added */ 
+ const spinner = document.getElementById('spinner');
 
-    const calling = await fetch(`https://openapi.programming-hero.com/api/phones?search=${brandName?brandName:"iphone"}`)
-    const data = await calling.json();
-    // addedData(data.data.slice(0,5));
-
-    if(status){
-        addedData(data.data);
-    }else{
-        addedData(data.data.slice(0,5));
-    }
-}
-/* calling api and spiner 3 second added */
-const searchTwoBtn = () => {
-    document.getElementById('spinner').style.display = 'block';
-
-    const searchInput = document.getElementById('search-box').value;
-    // console.log(searchInput)
-
-   setTimeout(() => {
-    searchBtn(false, searchInput)
-   }, 2000);
-}
-/* console api data */
-const addedData = (apiData) => {
-
-    const phoneContainer = document.getElementById('phones-container');
-    apiData.forEach(data => {
-        const {brand, image, phone_name, slug} = data;
+ const searchTwoBtn = async() => {
+  const searchInput = document.getElementById('search-box').value;
+  
+    spinner.style.display = 'block';
+    setTimeout (() => {
+      spinner.style.display = 'none'
+      apiadded(searchInput);
+    }, 2000);
     
-        const createDiv = document.createElement('div');
-        createDiv.innerHTML = `
-        
-        <div class="card mt-8 bg-base-100 w-96 shadow-xl border-2">
+ }
+ /* api added */
+const apiadded = async(brand) => {
+  
+  const res = await fetch (`https://openapi.programming-hero.com/api/phones?search=${brand}`);
+  const data = await res.json();
+  apiCalling(data.data);
+};
+
+
+
+const apiCalling = (data) => {
+  const addedInputValue = document.getElementById('phones-container');
+  data.forEach(da => {
+    const {image, slug, brand, phone_name} = da;
+    // console.log(da)
+    const createDiv = document.createElement('div');
+    createDiv.innerHTML = `
+    
+    <div class="card bg-base-100 w-96 shadow-xl mb-5">
   <figure class="px-10 pt-10">
     <img
-      src=${image}
+      src="${image}"
       alt="Shoes"
       class="rounded-xl" />
   </figure>
   <div class="card-body items-center text-center">
     <h2 class="card-title">${brand}</h2>
-    <p class"text-xl">${phone_name}</p>
     <p>${slug}</p>
+    <p>${phone_name}</p>
     <div class="card-actions">
-      <button class="btn btn-primary">Show Details</button>
+      <button onclick = "showDetails('${slug}')" class="btn btn-primary">Buy Now</button>
     </div>
   </div>
 </div>
-        
-        `
-        phoneContainer.appendChild(createDiv);
+    
+    `
+    addedInputValue.appendChild(createDiv);
+  });
+  document.getElementById('search-box').value = " ";
+ 
+};
+
+const showDetails = async (slugs) => {
+  const res = await fetch (`https://openapi.programming-hero.com/api/phone/${slugs}`);
+  const data = await res.json();
+  // console.log(data.data);
+
+  const {brand, image, name, releaseDate, slug} = data.data
+ 
+  const modelContainer = document.getElementById('modal-container');
+
+  modelContainer.innerHTML = `
   
-    });
-    
-    
+  <dialog id="my_modal_1" class="modal">
+  <div class="modal-box justify-items-center items-center">
+  <h3 class="text-2xl font-extra-bold">${brand}</h3>
+  <img src="${image}" alt="">
+    <h3 class="text-lg font-bold">${name}</h3>
+    <p class="py-4">${releaseDate}</p>
+    <div class="modal-action">
+      <form method="dialog">
+        <!-- if there is a button in form, it will close the modal -->
+        <button class="btn">Close</button>
+      </form>
+    </div>
+  </div>
+</dialog>
+  
+  `
+
+  my_modal_1.showModal()
 }
 
-const handleShowAll = () => {
-    searchBtn(true);
-    document.getElementById('spinner').style.display = 'block';
-    setTimeout(() => {
-        document.getElementById('spinner').style.display = 'none';
-    }, 1500);
-}
+
+// const apiDaynamic = (organic) => {
+//   console.log(organic)
+// }
+
+// const handleShowAll = () => {
+//   apiCalling(true);
+// }
+
